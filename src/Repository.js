86ce639781adac86d474ad89a821ac9f8860391a
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2019-10-23 14:55:22 -0600

    Change name of file

---
+++ src/Repository.js
@@ -0,0 +1,19 @@
+class Repository {
+  constructor(users) {
+    this.users = users;
+  }
+
+  returnUserData(idNum) {
+    return this.users.find((currentElement) => currentElement.id === idNum);
+  }
+
+  returnAllUsersAverageStepGoal() {
+    let sum = this.users.reduce((acc, currentValue) => {
+      return acc + currentValue.dailyStepGoal;
+    }, 0);
+    return parseInt(sum / this.users.length)
+  }
+}
+
+
+export default Repository;
