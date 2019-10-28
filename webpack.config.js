5c4b81a39c6d16ccd1f4dfce07da4303fc28ffd8
James Miller <james.rex.miller.4@gmail.com>
2019-10-27 21:35:27 -0600

    Put me out of my misery

--- webpack.config.js
+++ webpack.config.js
@@ -23,8 +23,8 @@ module.exports = {
                 loader: 'file-loader',
                 options: {
                   name: '[name].[ext]',
-                  outputPath: './images',
-                  publicPath: './images'
+                  outputPath: './images/',
+                  publicPath: './images/'
                 }
               }
             ]
