4135418861a2b8917205d1094ad71b357cddae30
James Miller <james.rex.miller.4@gmail.com>
2019-10-30 06:30:47 -0600

    last try

--- main.bundle.js
+++ main.bundle.js
@@ -45430,10 +45430,10 @@ function generateRandomUserId() {
 function displaySleepStatus() {
   sleep.checkUserRestedByDate(user.findCurrentUserData(sleep.data), '2019/09/15')
   if (sleep.isRested === true) {
-    jquery__WEBPACK_IMPORTED_MODULE_0___default()('#sleep-status').attr({src: '../images/ghost-happy.svg', alt: 'happy ghost icon'});
+    jquery__WEBPACK_IMPORTED_MODULE_0___default()('#sleep-status').attr({src: 'ghost-happy.svg', alt: 'happy ghost icon'});
     jquery__WEBPACK_IMPORTED_MODULE_0___default()('#sleep-comment').text('You\'ve been getting enough sleep!');
   } else {
-    jquery__WEBPACK_IMPORTED_MODULE_0___default()('#sleep-status').attr({src: '../images/ghost-sad.svg', alt: 'sad ghost icon'});
+    jquery__WEBPACK_IMPORTED_MODULE_0___default()('#sleep-status').attr({src: 'ghost-sad.svg', alt: 'sad ghost icon'});
     jquery__WEBPACK_IMPORTED_MODULE_0___default()('#sleep-comment').text('Getting 8 hours of sleep will make you more productive!');
   }
 }
@@ -45441,10 +45441,10 @@ function displaySleepStatus() {
 function displayWaterStatus() {
   let checkWater = hydration.returnDidUserDrinkEnoughWater('numOunces', user, hydration.data)
   if (checkWater === true) {
-    jquery__WEBPACK_IMPORTED_MODULE_0___default()('#water-status').attr({src: '../images/glass-full.svg', alt: 'full water glass icon'});
+    jquery__WEBPACK_IMPORTED_MODULE_0___default()('#water-status').attr({src: 'glass-full.svg', alt: 'full water glass icon'});
     jquery__WEBPACK_IMPORTED_MODULE_0___default()('#water-comment').text('Keep up the good work! You\'ve averaged more than 64 ounces per day this week');
   } else {
-    jquery__WEBPACK_IMPORTED_MODULE_0___default()('#water-status').attr({src: '../images/glass-empty.svg', alt: 'empty water glass icon'});
+    jquery__WEBPACK_IMPORTED_MODULE_0___default()('#water-status').attr({src: 'glass-empty.svg', alt: 'empty water glass icon'});
     jquery__WEBPACK_IMPORTED_MODULE_0___default()('#water-comment').text('You need more water. Make sure you\'re staying hydrated!');
   }
 }
